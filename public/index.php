1e7f2207fc5fba9b6d39d1f443484f334848d145
Matt Hickling <m.hickling@hotmail.co.uk>
2025-11-25 20:31:05 +0000

    started adding login, register and forgotten pages

--- public/index.php
+++ public/index.php
@@ -1,11 +1,32 @@
 <?php
 session_start();
 
-if (!isset($_SESSION['user_id'])) {
-    header('Location: login.php');
+require '../config/config.php';
+
+$page = $_GET['page'] ?? 'login';
+
+if (!isset($_SESSION['user_id']) && $page !== 'login' && $page !== 'register' && $page !== 'forgotten') {
+    header('Location: /index.php?page=login');
     exit;
 }
 
+switch ($page) {
+    case 'login':
+        require '../templates/login-form.php';
+        break;
 
-require_once('../config/config.php');
-exit;
+    case 'register':
+        require '../templates/register-form.php';
+        break;
+
+    case 'forgotten':
+        require '../templates/forgotten-form.php';
+        break;
+
+    case 'dashboard':
+        require '../templates/dashboard.php';
+        break;
+
+    default:
+        echo 'Page not found';
+}
--- templates/register-form.php
+++ templates/register-form.php
@@ -6,29 +6,34 @@
     <link href="../public/assets/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../public/assets/css/font-awesome.css">
     <link rel="stylesheet" href="../public/assets/css/login.css">
-    <title>Login</title>
+    <title>Create Account</title>
 </head>
 <body class="bg-light">
 <div class="container p-5 d-flex flex-column align-items-center">
 
     <?php if (!empty($message)): ?>
-        <div class="toast align-items-center text-white border-0"
+        <div class="toast align-items-center text-white border-0" 
              role="alert" aria-live="assertive" aria-atomic="true"
-             style="background-color: <?php echo $toastClass; ?>;">
+             style="background-color: <?php echo $toastClass ?? '#28a745'; ?>;">
             <div class="d-flex">
                 <div class="toast-body"><?php echo $message; ?></div>
-                <button type="button" class="btn-close btn-close-white me-2 m-auto"
+                <button type="button" class="btn-close btn-close-white me-2 m-auto" 
                         data-bs-dismiss="toast" aria-label="Close"></button>
             </div>
         </div>
-    <?php endif; 
-    ?>
+    <?php endif; ?>
 
-    <form method="post" class="form-control mt-5 p-4" 
+    <form method="post" class="form-control mt-5 p-4"
           style="width:380px; box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
-                 rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;" action="dashboard.php">
+                 rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;">
         <div class="text-center mb-3">
-            <h5 class="p-2" style="font-weight: 700;">Login</h5>
+            </i>
+            <h5 class="p-2" style="font-weight: 700;">Create Your Account</h5>
+        </div>
+
+        <div class="mb-2">
+            <label for="username"></i> Username</label>
+            <input type="text" name="username" id="username" class="form-control" required>
         </div>
 
         <div class="mb-2">
@@ -41,19 +46,16 @@
             <input type="password" name="password" id="password" class="form-control" required>
         </div>
 
-        <button type="submit" class="btn btn-success w-100 mt-3">Login</button>
+        <button type="submit" class="btn btn-success w-100 mt-3">Create Account</button>
 
         <p class="text-center mt-3" style="font-weight: 600;">
-            <a href="index.php?page=register">Create Account</a> | 
-            <a href="index.php?page=forgotten">Forgot Password</a>
+            Already have an account? <a href="./login.php">Login</a>
         </p>
-
     </form>
 </div>
 
 <script src="../public/assets/js/jquery-3.6.0.min.js"></script>
 <script src="../public/assets/js/bootstrap.bundle.min.js"></script>
-
 <script>
     let toastElList = [].slice.call(document.querySelectorAll('.toast'))
     let toastList = toastElList.map(function(toastEl) {
