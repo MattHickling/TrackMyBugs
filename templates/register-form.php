c9e99c7efbfae549497d02b8ce132e8a4b23e84f
Matt Hickling <m.hickling@hotmail.co.uk>
2025-11-26 19:24:31 +0000

    forgotten password page added and links between pages done

--- templates/register-form.php
+++ templates/register-form.php
@@ -49,7 +49,7 @@
         <button type="submit" class="btn btn-success w-100 mt-3">Create Account</button>
 
         <p class="text-center mt-3" style="font-weight: 600;">
-            Already have an account? <a href="./login.php">Login</a>
+            Already have an account? <a href="index.php?page=login">Login</a>
         </p>
     </form>
 </div>
