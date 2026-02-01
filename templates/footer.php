6610a235d1fe3e6e523c9694875733e9482b23bc
Matt Hickling <m.hickling@hotmail.co.uk>
2026-02-01 19:43:53 +0000

    small typos

--- templates/footer.php
+++ templates/footer.php
@@ -3,13 +3,13 @@
     
   </div>
   <div class="card-body">
-    <h5 class="card-title">Track My Bugs</h5>
+    <h5 class="card-title">TrackMyBugs</h5>
     <p class="card-text"> Manage your projects and bugs efficiently.</p>
     <a href="<?= APP_BASE_URL ?>/dashboard.php" class="btn btn-primary">Dashboard</a>
     <a href="<?= APP_BASE_URL ?>/profile.php" class="btn btn-primary">Profile</a>
     <a href="<?= APP_BASE_URL ?>/logout.php" class="btn btn-primary">Logout</a>
   </div>
   <div class="card-footer text-muted">
-    &copy; <?= date('Y') ?> Track My Bugs
+    &copy; <?= date('Y') ?> TrackMyBugs
   </div>
 </div>
